0ef71cd1fc4754a395e30ef4ed95d1667d312aad
menady0 <menady20@gmail.com>
2024-11-22 23:47:34 +0200

    added footer to about-us

--- src/app/Components/about/about.component.ts
+++ src/app/Components/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FooterComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
