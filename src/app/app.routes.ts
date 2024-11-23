6c2dfb52d20c349fb7a7dbc8fa8117a2a7676c1a
menady0 <menady20@gmail.com>
2024-11-23 10:11:56 +0200

    about us is done and responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component';
 import { CoursesComponent } from './Components/courses/courses.component';
 import { LoginComponent } from './Components/login/login.component';
+import { AboutComponent } from './Components/about/about.component';
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'home', component: HomeComponent},
-  {path: 'courses', component: CoursesComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'home', title: 'Learny. | Learn Whatever You Want At Anytime', component: HomeComponent},
+  {path: 'courses', title: 'Learny. | Courses', component: CoursesComponent},
+  {path: 'login', title: 'Learny. | Join Us', component: LoginComponent},
+  {path: 'about', title: 'Learny. | About Us', component: AboutComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
+  // {path: "**", title: "404", component: PageNotFoundComponent},
 ];
